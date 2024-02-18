49e051dc0a221260c8e58a1d03736793ec74beeb
quang.nt <ntq20803@gmail.com>
2024-02-18 09:58:28 +0700

    EDIT keymap configs for windows navigations

--- lua/global-keymap.lua
+++ lua/global-keymap.lua
@@ -1,9 +1,15 @@
 -- save file
-vim.keymap.set('n', '<C-s>', ':w<CR>', {})
+vim.keymap.set({"n", "i"}, '<C-s>', '<esc>:w<CR>', {})
+-- split windows
+vim.keymap.set("n", "<A-s>", ":split<CR>", {})
+vim.keymap.set("n", "<A-v>", ":vsplit<CR>", {})
 -- move between windows
-vim.keymap.set("n", "<A-h>", "<C-w>h", {})
-vim.keymap.set("n", "<A-l>", "<C-w>l", {})
-vim.keymap.set("n", "<A-j>", "<C-w>j", {})
-vim.keymap.set("n", "<A-k>", "<C-w>k", {})
+vim.keymap.set("n", "<C-h>", "<C-w>h", {})
+vim.keymap.set("n", "<C-l>", "<C-w>l", {})
+vim.keymap.set("n", "<C-j>", "<C-w>j", {})
+vim.keymap.set("n", "<C-k>", "<C-w>k", {})
+-- move a line
 vim.keymap.set({'n', 'v'}, '<S-up>', ':m -2<CR>', {})
 vim.keymap.set({'n', 'v'}, '<S-down>', ':m +1<CR>', {})
+-- duplicate a line
+vim.keymap.set({'n', 'i'}, "<C-D>", 'yyp', {})
